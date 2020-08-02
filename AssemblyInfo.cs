788c0db1ba3107474e96e2f3ab3ce95285590856
ImTiara <39366953+ImTiara@users.noreply.github.com>
2020-08-02 14:59:13 +0200

    Initial commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(NoPerformanceStats.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(NoPerformanceStats.BuildInfo.Company)]
+[assembly: AssemblyProduct(NoPerformanceStats.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + NoPerformanceStats.BuildInfo.Author)]
+[assembly: AssemblyTrademark(NoPerformanceStats.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(NoPerformanceStats.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(NoPerformanceStats.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(NoPerformanceStats.NoPerformanceStats), NoPerformanceStats.BuildInfo.Name, NoPerformanceStats.BuildInfo.Version, NoPerformanceStats.BuildInfo.Author, NoPerformanceStats.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonModGame("VRChat", "VRChat")]
---
+++ NoPerformanceStats.cs
@@ -0,0 +1,65 @@
+﻿using Harmony;
+using MelonLoader;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace NoPerformanceStats
+{
+
+    public static class BuildInfo
+    {
+        public const string Name = "NoPerformanceStats";
+        public const string Author = "ImTiara";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class NoPerformanceStats : MelonMod
+    {
+
+        private HarmonyInstance harmonyInstance;
+        private static bool allowPerformanceScanner;
+
+        public override void OnApplicationStart()
+        {
+            ModPrefs.RegisterCategory("NoPerformanceStats", "No Performance Stats");
+            ModPrefs.RegisterPrefBool("NoPerformanceStats", "Disable Performance Stats", true, "");
+
+            LoadModPrefs();
+
+            harmonyInstance = HarmonyInstance.Create("NoPerformanceStatsPatcher");
+
+            try
+            {
+                MethodInfo[] methods = typeof(ObjectPublicAbstractSealedInObInObObObObUnique).GetMethods(BindingFlags.Public | BindingFlags.Static);
+                for (int i = 0; i < methods.Length; i++)
+                    if (methods[i].Name == "Method_Public_Static_IEnumerator_String_GameObject_AvatarPerformanceStats_0" || methods[i].Name == "Method_Public_Static_IEnumerator_GameObject_AvatarPerformanceStats_EnumPublicSealedvaNoExGoMePoVe7vUnique_MulticastDelegateNPublicSealedVoUnique_0" || methods[i].Name == "Method_Public_Static_Void_String_GameObject_AvatarPerformanceStats_0")
+                        harmonyInstance.Patch(methods[i], new HarmonyMethod(typeof(NoPerformanceStats).GetMethod("CalculatePerformance", BindingFlags.Static | BindingFlags.NonPublic)), null, null);
+            }
+            catch(Exception e)
+            {
+                MelonModLogger.Log(ConsoleColor.Red, "Failed to patch Performance Scanners: " + e);
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.P))
+            {
+                ModPrefs.SetBool("NoPerformanceStats", "Disable Performance Stats", allowPerformanceScanner);
+                LoadModPrefs();
+                MelonModLogger.Log("Avatar Performance Stats is now " + (allowPerformanceScanner ? "ENABLED" : "DISABLED"));
+            }
+        }
+
+        public override void OnModSettingsApplied() => LoadModPrefs();
+
+        private void LoadModPrefs() => allowPerformanceScanner = !ModPrefs.GetBool("NoPerformanceStats", "Disable Performance Stats");
+
+        private static bool CalculatePerformance() => allowPerformanceScanner;
+
+    }
+
+}

c80c467681e1df578912a98a0a7c07be15ce54b1
ImTiara <im.tiara@hotmail.com>
2021-06-17 13:05:30 +0200

    Fixes and improvements

--- NoPerformanceStats.cs
+++ NoPerformanceStats.cs
@@ -1,8 +1,10 @@
-﻿using Harmony;
+﻿using HarmonyLib;
 using MelonLoader;
 using System;
+using System.Collections;
 using System.Reflection;
-using VRCSDK2.Validation.Performance;
+using UnityEngine.UI;
+using VRC.SDKBase.Validation.Performance;
 
 namespace NoPerformanceStats
 {
@@ -11,45 +13,75 @@ namespace NoPerformanceStats
         public const string Name = "NoPerformanceStats";
         public const string Author = "ImTiara";
         public const string Company = null;
-        public const string Version = "1.0.3";
+        public const string Version = "1.0.4";
         public const string DownloadLink = "https://github.com/ImTiara/NoPerformanceStats/releases";
     }
 
     public class NoPerformanceStats : MelonMod
     {
-
-        private HarmonyInstance _harmonyInstance;
+        private static HarmonyLib.Harmony _harmonyInstance = new HarmonyLib.Harmony("NoPerformanceStatsPatcher");
 
         private static bool allowPerformanceScanner;
 
+        private static Text avatarStatsButtonText;
+
         public override void OnApplicationStart()
         {
-            _harmonyInstance = HarmonyInstance.Create("NoPerformanceStatsPatcher");
-
             MelonPreferences.CreateCategory(GetType().Name, "No Performance Stats");
             MelonPreferences.CreateEntry(GetType().Name, "DisablePerformanceStats", true, "Disable Performance Stats");
 
-            LoadModPrefs();
+            ApplyPatches();
 
+            MelonCoroutines.Start(UiManagerInitializer());
+        }
+
+        private void OnUiManagerInit()
+        {
+            avatarStatsButtonText = VRCUiManager.prop_VRCUiManager_0.transform.Find("MenuContent/Screens/Avatar/Stats Button/Text").GetComponent<Text>();
+
+            OnPreferencesSaved();
+        }
+
+        public override void OnPreferencesSaved()
+        {
+            allowPerformanceScanner = !MelonPreferences.GetEntryValue<bool>(GetType().Name, "DisablePerformanceStats");
+
+            RefreshPerfStuff();
+        }
+
+        private void ApplyPatches()
+        {
             try
             {
-                _harmonyInstance.Patch(
-                    typeof(AvatarPerformance).GetMethod(nameof(AvatarPerformance.Method_Public_Static_IEnumerator_String_GameObject_AvatarPerformanceStats_0), BindingFlags.Public | BindingFlags.Static),
-                    new HarmonyMethod(typeof(NoPerformanceStats).GetMethod(nameof(NoPerformanceStats.CalculatePerformance), BindingFlags.Static | BindingFlags.NonPublic)), null, null
+                Patch(
+                    typeof(PerformanceScannerSet),
+                    nameof(PerformanceScannerSet.Method_Public_IEnumerator_GameObject_AvatarPerformanceStats_MulticastDelegateNPublicSealedBoCoUnique_0),
+
+                    typeof(NoPerformanceStats),
+                    nameof(CalculatePerformance),
+
+                    BindingFlags.Public | BindingFlags.Instance,
+                    BindingFlags.NonPublic | BindingFlags.Static
                 );
             }
-            catch(Exception e) { MelonLogger.Error("Failed to patch Performance Scanner: " + e); }
+            catch (Exception e) { MelonLogger.Error("Failed to patch Performance Scanner: " + e); }
         }
-        
-        public override void OnPreferencesSaved()
-            => LoadModPrefs();
 
-        private void LoadModPrefs()
-            => allowPerformanceScanner = !MelonPreferences.GetEntryValue<bool>(GetType().Name, "DisablePerformanceStats");
+        private static void RefreshPerfStuff()
+        {
+            avatarStatsButtonText.text = allowPerformanceScanner ? "Avatar Stats" : "<color=#ff6464>Stats Disabled!</color>";
+        }
 
         private static bool CalculatePerformance()
             => allowPerformanceScanner;
 
+        private static void Patch(Type targetClass, string target, Type detourClass, string detour, BindingFlags targetBindingFlags = BindingFlags.Public | BindingFlags.Instance, BindingFlags detourBindingFlags = BindingFlags.NonPublic | BindingFlags.Static)
+            => _harmonyInstance.Patch(targetClass.GetMethod(target, targetBindingFlags), new HarmonyMethod(detourClass.GetMethod(detour, detourBindingFlags)), null, null);
+        
+        private IEnumerator UiManagerInitializer()
+        {
+            while (VRCUiManager.prop_VRCUiManager_0 == null) yield return null;
+            OnUiManagerInit();
+        }
     }
-
 }
